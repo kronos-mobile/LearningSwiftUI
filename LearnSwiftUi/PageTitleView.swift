130094dc6ea9505d1b412f6d2d64866099c22c2f
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2020-04-25 18:05:43 -0500

    state variables and buttons

--- LearnSwiftUi/PageTitleView.swift
+++ LearnSwiftUi/PageTitleView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct PageTitleView: View {
     var title: String
+    var isDisplayingOrders: Bool! = nil
     var body: some View {
         HStack {
             Spacer()
@@ -17,8 +18,9 @@ struct PageTitleView: View {
                 .fontWeight(.heavy)
                 .padding(.trailing)
         }.overlay(
-            Image(systemName: "chevron.up.square")
+            Image(systemName: isDisplayingOrders ?? false ? "chevron.up.square" : "chevron.down.square")
                 .font(.title)
+                .foregroundColor(isDisplayingOrders != nil ? Color("G1") : .clear)
             .padding(),
             alignment: .leading
         )
@@ -29,6 +31,6 @@ struct PageTitleView: View {
 
 struct PageTitleView_Previews: PreviewProvider {
     static var previews: some View {
-        PageTitleView(title: "Order Pizza")
+        PageTitleView(title: "Order Pizza!")
     }
 }
