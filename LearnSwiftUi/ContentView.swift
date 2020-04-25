737d1dd1444139beeb24dc7988a361f77e302dbb
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2020-04-25 10:06:33 -0500

    basic lists

--- LearnSwiftUi/ContentView.swift
+++ LearnSwiftUi/ContentView.swift
@@ -9,12 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ZStack {
+                Image("Surf Board")
+                    .resizable()
+                    .scaledToFit()
+                Text("Huli Pizza Company")
+                    .font(.title)
+            }
+            
+            Text("Order Pizza")
+                .font(.largeTitle)
+            
+            Text("Menu")
+            
+            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                HStack(alignment: .top, spacing: 15) {
+                    Image("1_100w")
+                    Text("Huli Chicken Pizza")
+                    Spacer()
+                }
+            }
+            
+            Text("Your Order")
+            
+            List(0..<5) {item in
+                HStack(alignment: .firstTextBaseline) {
+                    Text("Your order item here")
+                    Spacer()
+                    Text("$0.00")
+                }
+            }
+            
+            Spacer()
+            
+        }.padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+//            ContentView()
+//                .colorScheme(.dark)
+//                .background(Color.black)
+//            .previewDevice("iPad Pro (9.7-inch)")
+        }
     }
 }
+
