aed5d5e4a5ab5705b1d351ec6331ccbf6b4f09b3
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2020-04-25 13:12:20 -0500

    SF symbols

--- LearnSwiftUi/MenuView/MenuRowView.swift
+++ LearnSwiftUi/MenuView/MenuRowView.swift
@@ -11,7 +11,7 @@ struct MenuRowView: View {
     var body: some View {
         HStack(alignment: .top, spacing: 15) {
             Image("1_100w")
-            VStack {
+            VStack(alignment: .leading) {
                 Text("Huli Chicken Pizza")
                     .font(.title)
                     .fontWeight(.light)
@@ -26,6 +26,6 @@ struct MenuRowView: View {
 struct MenuRowView_Previews: PreviewProvider {
     static var previews: some View {
         MenuRowView()
-            .environment(\.sizeCategory, .accessibilityExtraLarge)
+//            .environment(\.sizeCategory, .accessibilityExtraLarge)
     }
 }
