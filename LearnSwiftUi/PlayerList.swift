c9e336eb737aa430ef873b048d882b6907194c52
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2020-05-26 11:14:19 -0500

    added player list

---
+++ LearnSwiftUi/PlayerList.swift
@@ -0,0 +1,27 @@
+//
+//  PlayerList.swift
+//  LearnSwiftUi
+//
+//  Created by Vanessa Milagros on 5/26/20.
+//
+
+import SwiftUI
+
+struct PlayerList: View {
+    var body: some View {
+        NavigationView {
+            List(players) {
+                player in
+                NavigationLink(destination: ContentView()) {
+                    PlayerRow(player: player)
+                }
+            }.navigationBarTitle(Text("NBA Finals Players"), displayMode: .large)
+        }
+    }
+}
+
+struct PlayerList_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerList()
+    }
+}
