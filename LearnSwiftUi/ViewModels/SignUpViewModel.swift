8a726f19040f8017e622de72c5c325720a4b0e0b
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2020-06-04 07:05:08 -0500

    binding a sign up layout using MVVM + combine - episode 3

--- LearnSwiftUi/ViewModels/SignUpViewModel.swift
+++ LearnSwiftUi/ViewModels/SignUpViewModel.swift
@@ -4,13 +4,13 @@ import Combine
 class SignUpViewModel: ObservableObject {
     private var cancellableBag = Set<AnyCancellable>()
     
-    @Published var username: String = "Test"
+    @Published var username: String = ""
     var usernameError: String = "Required"
-    @Published var email: String = "test@mail.com"
+    @Published var email: String = ""
     var emailError: String = "Required"
-    @Published var password: String = "123"
+    @Published var password: String = ""
     var passwordError: String = "Required"
-    @Published var confirmPassword: String = "123"
+    @Published var confirmPassword: String = ""
     var confirmPasswordError: String = "Required"
     
     private var usernameValidPublisher: AnyPublisher<Bool, Never> {
@@ -20,6 +20,49 @@ class SignUpViewModel: ObservableObject {
             .eraseToAnyPublisher()
     }
     
+    private var emailRequiredPublisher: AnyPublisher<(email: String, isValid: Bool), Never> {
+            return $email
+                .map{ (email: $0, isValid: !$0.isEmpty) }
+                .eraseToAnyPublisher()
+    }
+    
+    private var emailValidPublisher: AnyPublisher<(email: String, isValid: Bool), Never> {
+        return emailRequiredPublisher
+            .filter { $0.isValid }
+            .map { (email: $0.email, isValid: $0.email.isValidEmail()) }
+            .eraseToAnyPublisher()
+    }
+    
+    private var passwordRequiredPublisher: AnyPublisher<(password: String, isValid: Bool), Never> {
+            return $password
+                .map{ (passowrd: $0, isValid: !$0.isEmpty) }
+                .eraseToAnyPublisher()
+    }
+    
+    private var passwordValidPublisher: AnyPublisher<Bool, Never> {
+        return passwordRequiredPublisher
+            .filter { $0.isValid }
+            .map { $0.password.isValidPassword() }
+            .eraseToAnyPublisher()
+    }
+    
+    private var confirmPasswordRequiredPublisher: AnyPublisher<(password: String, isValid: Bool), Never> {
+            return $password
+                .map{ (passowrd: $0, isValid: !$0.isEmpty) }
+                .eraseToAnyPublisher()
+    }
+    
+    private var passwordEqualPublisher: AnyPublisher<Bool, Never> {
+        return Publishers.CombineLatest($password, $confirmPassword)
+            .filter { !$0.0.isEmpty && !$0.1.isEmpty }
+            .map { password, confirm in
+                return password == confirm
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    
+    
     init() {
         usernameValidPublisher
             .receive(on: RunLoop.main)
@@ -27,5 +70,65 @@ class SignUpViewModel: ObservableObject {
             .map { $0 ? "": "Username is missing" }
             .assign(to: \.usernameError, on: self)
             .store(in: &cancellableBag)
+        
+        emailRequiredPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0.isValid ? "": "Email is missing" }
+            .assign(to: \.emailError, on: self)
+            .store(in: &cancellableBag)
+        
+        emailValidPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0.isValid ? "": "Email is not valid" }
+            .assign(to: \.emailError, on: self)
+            .store(in: &cancellableBag)
+        
+        passwordRequiredPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0.isValid ? "": "Password is missing" }
+            .assign(to: \.passwordError, on: self)
+            .store(in: &cancellableBag)
+        
+        passwordValidPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0 ? "": "Password must be 8 characters with 1 alphabet and 1 number" }
+            .assign(to: \.passwordError, on: self)
+            .store(in: &cancellableBag)
+        
+        confirmPasswordRequiredPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0.isValid ? "": "Confirm Password is missing" }
+                .assign(to: \.confirmPasswordError, on: self)
+            .store(in: &cancellableBag)
+        
+        passwordEqualPublisher
+            .receive(on: RunLoop.main)
+            .map { $0 ? "": "Passwords does not match" }
+                .assign(to: \.confirmPasswordError, on: self)
+            .store(in: &cancellableBag)
+    }
+    
+    
+    deinit {
+        cancellableBag.removeAll()
+    }
+}
+
+extension String {
+    func isValidEmail() -> Bool {
+        // https://regexr.com/
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: self)
+    }
+    
+    func isValidPassword(pattern: String = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$") -> Bool {
+        let passwordRegex = pattern
+        return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self)
     }
 }
